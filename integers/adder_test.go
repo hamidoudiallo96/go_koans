0cca30e12f7b5544b1f46b919c2af00b69fa58df
Hamidou Diallo <dio.hamidou@gmail.com>
2026-05-13 02:32:49 -0400

    Add repeat file and corresponding test file

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,10 +1,15 @@
 package integers
 
-import "testing"
+import(
+	"testing"
+	"fmt"
+)
 
 func TestAdder(t *testing.T) {
 	got := Add(81, 729)
+	fmt.Println(got)
 	want := 810
+	// Output: 6
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
