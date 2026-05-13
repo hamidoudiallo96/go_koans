dbd3bd159622495404f6237542d31c325e5ed2c7
Hamidou Diallo <dio.hamidou@gmail.com>
2026-05-13 02:35:08 -0400

    Revert "Add repeat file and corresponding test file"
    This reverts commit 0cca30e12f7b5544b1f46b919c2af00b69fa58df.

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,15 +1,10 @@
 package integers
 
-import(
-	"testing"
-	"fmt"
-)
+import "testing"
 
 func TestAdder(t *testing.T) {
 	got := Add(81, 729)
-	fmt.Println(got)
 	want := 810
-	// Output: 6
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
