574b9ee963ca0d76243457986ca1fec92399a0ab
Hamidou Diallo <dio.hamidou@gmail.com>
2026-05-20 18:18:33 -0400

    Refactor table driven tests to include additional fields

--- structs/shape_test.go
+++ structs/shape_test.go
@@ -28,18 +28,19 @@ func TestArea(t *testing.T) {
 
 	// Table Driven Tests
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{7.0, 13.0, 5.0, 12.0}, 30.0},
+		{name: "rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "triangle", shape: Triangle{SideA: 7.0, SideB: 13.0, Base: 5.0, Height: 12.0}, hasArea: 30.0},
 	}
 
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
 }
@@ -70,18 +71,19 @@ func TestPerimeter(t *testing.T) {
 
 	// Table Driven Test
 	perimeterTests := []struct {
-		shape Shape
-		want  float64
+		name         string
+		shape        Shape
+		hasPerimeter float64
 	}{
-		{Rectangle{12.0, 9.0}, 42.0},
-		{Circle{19.25}, 120.95131716320704},
-		{Triangle{7.0, 13.0, 5.0, 12.0}, 25.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 9.0}, hasPerimeter: 42.0},
+		{name: "Circle", shape: Circle{Radius: 19.25}, hasPerimeter: 120.95131716320704},
+		{name: "Triangle", shape: Triangle{SideA: 7.0, SideB: 13.0, Base: 5.0, Height: 12.0}, hasPerimeter: 25.0},
 	}
 
 	for _, tt := range perimeterTests {
 		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasPerimeter {
+			t.Errorf("got %g want %g", got, tt.hasPerimeter)
 		}
 	}
 }
